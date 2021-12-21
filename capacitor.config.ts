6fc9ffb2c006c340e65e5c300b9f200fec62ab83
FreyaLopez <69055056+FreyaLopez@users.noreply.github.com>
2021-12-21 14:57:12 -0500

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tarea_1-2_hojadevida_FreyaLopez',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
